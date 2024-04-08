811efb6d7754754abb8131d1404d6367d1fb533c
jayakumar.velayudham <jayakumar.velayudham@ezlo.com>
2024-04-08 19:32:20 +0530

    Test case updated

--- app/src/test/java/com/product/groceryapplication/viewmodel/cart/CartViewModelTest.kt
+++ app/src/test/java/com/product/groceryapplication/viewmodel/cart/CartViewModelTest.kt
@@ -5,13 +5,11 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth
 import com.product.groceryapplication.MainCoroutineRule
 import com.product.groceryapplication.ui.cart.CartViewModel
-import com.product.groceryapplication.ui.details.ProductDetailsViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.jupiter.api.BeforeEach
 
 class CartViewModelTest {
     private lateinit var viewModel: CartViewModel
